368433c1e12d4c1e4f504323e33b3030743b8e0b
Chain Chonchanok Wongaphai <chonchanok@nipa.cloud>
2024-11-11 22:20:03 +0700

    edit gap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className='min-h-screen bg-gray-50 flex flex-col justify-center items-center'>
+    <div className='min-h-screen bg-gray-50 flex flex-col justify-center items-center p-8'>
       <div className='max-w-3xl text-center space-y-8'>
         <h1 className='text-4xl md:text-5xl font-bold text-blue-600'>Welcome to JobFair</h1>
         <p className='text-lg md:text-xl text-gray-700 leading-relaxed'>
@@ -22,16 +22,16 @@ export default function Home() {
           </Link>
         </div>
       </div>
-      <div className='mt-16 w-full max-w-5xl flex flex-wrap justify-center space-x-8'>
-        <div className='text-center p-6 bg-white shadow-lg rounded-lg w-64 mt-6'>
+      <div className='mt-16 w-full max-w-5xl flex flex-wrap justify-center gap-8'>
+        <div className='text-center p-6 bg-white shadow-lg rounded-lg w-64'>
           <h2 className='text-2xl font-semibold text-blue-600'>ค้นหางาน</h2>
           <p className='text-gray-700 mt-2'>ค้นหาโอกาสงานที่เหมาะกับคุณจากบริษัทหลากหลายที่ร่วมเข้าร่วมกับเรา</p>
         </div>
-        <div className='text-center p-6 bg-white shadow-lg rounded-lg w-64 mt-6'>
+        <div className='text-center p-6 bg-white shadow-lg rounded-lg w-64'>
           <h2 className='text-2xl font-semibold text-blue-600'>ค้นหาบริษัท</h2>
           <p className='text-gray-700 mt-2'>สำรวจบริษัทต่างๆ และเรียนรู้เกี่ยวกับโอกาสและวิสัยทัศน์ของแต่ละองค์กร</p>
         </div>
-        <div className='text-center p-6 bg-white shadow-lg rounded-lg w-64 mt-6'>
+        <div className='text-center p-6 bg-white shadow-lg rounded-lg w-64'>
           <h2 className='text-2xl font-semibold text-blue-600'>ลงทะเบียนผู้สมัคร</h2>
           <p className='text-gray-700 mt-2'>สมัครสมาชิกและสร้างโปรไฟล์เพื่อเพิ่มโอกาสในการหางานที่เหมาะกับคุณ</p>
         </div>
